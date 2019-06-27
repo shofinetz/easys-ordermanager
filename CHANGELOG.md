86e537f8e2e6d61343f3a5a22f5c6132fc950468
Vladimir Potter <vladimir.potter@regiohelden.de>
2019-06-27 18:33:28 +0200

    Include missing files to the package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.0.1 (2019-06-27)
+
+* Add missing files to the package
+
+
 ## 1.0.0 (2019-06-24)
 
 * Initial release
--- easys_ordermanager/__init__.py
+++ easys_ordermanager/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'easys-ordermanager'
 __description__ = 'API definition of RH order manager for EasyS'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __url__ = 'https://github.com/RegioHelden/easys-ordermanager'
 __author__ = 'Jens Nistler, Mounir Messelmeni'
 __author_email__ = 'opensource@regiohelden.de'
