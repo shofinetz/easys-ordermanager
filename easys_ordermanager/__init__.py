40808df761f39b5817d7cd636a389924b58763d4
Mihaela Sofinet <tamara.sofinet@gmail.com>
2020-04-02 11:28:05 +0200

    Bump version: 1.3.1 → 1.3.2

--- easys_ordermanager/__init__.py
+++ easys_ordermanager/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'easys-ordermanager'
 __description__ = 'API definition of RH order manager for EasyS'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 __url__ = 'https://github.com/RegioHelden/easys-ordermanager'
 __author__ = 'Jens Nistler, Mounir Messelmeni'
 __author_email__ = 'opensource@regiohelden.de'
