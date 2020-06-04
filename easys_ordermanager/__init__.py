f26d20839ff47a61e69dbc1269359737956c2d6e
Jens Nistler <jens.nistler@regiohelden.de>
2020-06-04 09:29:00 +0200

    Bump version: 1.4.3 → 1.4.4

--- easys_ordermanager/__init__.py
+++ easys_ordermanager/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'easys-ordermanager'
 __description__ = 'API definition of RH order manager for EasyS'
-__version__ = '1.4.3'
+__version__ = '1.4.4'
 __url__ = 'https://github.com/RegioHelden/easys-ordermanager'
 __author__ = 'Jens Nistler, Mounir Messelmeni'
 __author_email__ = 'opensource@regiohelden.de'
