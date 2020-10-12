ff94e23b4ffd8b92d493a5d4b35dc4091ef433dc
sofi <mihaela.sofinet@regiohelden.de>
2020-10-12 18:23:20 +0200

    Test bumping version commit

--- easys_ordermanager/__init__.py
+++ easys_ordermanager/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'easys-ordermanager'
 __description__ = 'API definition of RH order manager for EasyS'
-__version__ = '1.4.13'
+__version__ = '1.4.14'
 __url__ = 'https://github.com/RegioHelden/easys-ordermanager'
 __author__ = 'Jens Nistler, Mounir Messelmeni'
 __author_email__ = 'opensource@regiohelden.de'
